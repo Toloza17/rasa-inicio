ccd66903a56cdbf42ed0a7e6a20be5d4dfac93c2
JoseToloza <josetoloza8@gmail.com>
2024-07-18 00:15:59 -0400

    rasa 100% funcional
    se le quito todo lo del menu ya que se hara desde django y luego se redirecciona a rasa (en implementacion) el rasa tiene mas ejemplos para afrontar mas casos inusuales y mayor tolerancia

--- actions/actions.py
+++ actions/actions.py
@@ -7,16 +7,13 @@ class ActionPreguntarProblema(Action):
     def name(self) -> Text:
         return "action_preguntar_problema"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_preguntar_problemas")
-        return []
-
+ 
 class ActionDefaultFallback(Action):
     def name(self) -> Text:
         return "action_default_fallback"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_default")
+        dispatcher.utter_message(response="utter_default")
         return []
 
 class SoloNumerosYBlanco(Action):
@@ -28,11 +25,11 @@ class SoloNumerosYBlanco(Action):
         words = user_message.split()
         
         if not user_message:
-            dispatcher.utter_message(template="utter_ask_valid_question")
+            dispatcher.utter_message(response="utter_ask_valid_question")
         else:
             num_count = sum(1 for word in words if word.isdigit())
             if num_count / len(words) >= 0.5:
-                dispatcher.utter_message(template="utter_ask_invalid_number_question")
+                dispatcher.utter_message(response="utter_ask_invalid_number_question")
             else:
                 dispatcher.utter_message("")
         return []
@@ -44,28 +41,7 @@ class MensajeEnBlanco(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         user_message = tracker.latest_message.get("text", "").strip()
         if not user_message:
-            dispatcher.utter_message(template="utter_ask_valid_question")
+            dispatcher.utter_message(response="utter_ask_valid_question")
         else:
             dispatcher.utter_message("")
-        return []
-
-class ActionGuardarRespuesta(Action):
-    def name(self) -> Text:
-        return "action_guardar_respuesta"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        problem_type = tracker.get_slot("problem_type")
-        question = "¿Qué tipo de problema tienes?"
-        response = problem_type
-        is_correct = True  # La respuesta es correcta o no
-
-        data = {
-            "question": question,
-            "response": response,
-            "is_correct": is_correct
-        }
-
-        response = requests.post("http://localhost:8000/rasadj/action/", data=data)
-
-        dispatcher.utter_message(template="utter_seleccionar_problema", problem_type=problem_type)
         return []
